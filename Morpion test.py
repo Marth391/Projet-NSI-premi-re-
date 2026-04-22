006f53b6aca03630a0bb394782c9b364005284a0
Tikiwi39 <cueninerwann@gmail.com>
2026-04-22 11:14:55 +0200

    Update Morpion test.py

--- Morpion test.py
+++ Morpion test.py
@@ -1,4 +1,6 @@
-plateau =[0 for i in range 9]
+import random as rd
+plateau=[0 for i in range(9)]
+
 
 def choix_joueur():
     case=int(input("Entrer un nombre entre 1 et 9: "))
@@ -10,41 +12,44 @@ def choix_joueur():
         return none
     print(plateau)
 
-def bot ():
-	bloque la victoire du joueur ou joue une case au hasard
+#def bot ():
+    
+    
 
 def jeu():
+    choix_joueur
     win=None
-    if  plateau[case]== plateau[0]==plateau[1]==plateau[2]\
-    or  plateau[case]== plateau[3]==plateau[4]==plateau[5]\
-    or  plateau[case]== plateau[6]==plateau[7]==plateau[8]\
-    or  plateau[case]== plateau[0]==plateau[3]==plateau[6]\
-    or  plateau[case]== plateau[1]==plateau[4]==plateau[7]\
-    or  plateau[case]== plateau[2]==plateau[5]==plateau[8]\
-    or  plateau[case]== plateau[0]==plateau[4]==plateau[8]\
-    or  plateau[case]== plateau[6]==plateau[4]==plateau[2]:
+    if  plateau[case]==plateau[0]==plateau[1]==plateau[2]\
+    or  plateau[case]==plateau[3]==plateau[4]==plateau[5]\
+    or  plateau[case]==plateau[6]==plateau[7]==plateau[8]\
+    or  plateau[case]==plateau[0]==plateau[3]==plateau[6]\
+    or  plateau[case]==plateau[1]==plateau[4]==plateau[7]\
+    or  plateau[case]==plateau[2]==plateau[5]==plateau[8]\
+    or  plateau[case]==plateau[0]==plateau[4]==plateau[8]\
+    or  plateau[case]==plateau[6]==plateau[4]==plateau[2]:
         win=True
     
-    if  == plateau[0]==plateau[1]==plateau[2]\
-    or  plateau[case_bot]== plateau[3]==plateau[4]==plateau[5]\
-    or  plateau[case_bot]== plateau[6]==plateau[7]==plateau[8]\
-    or  plateau[case_bot]== plateau[0]==plateau[3]==plateau[6]\
-    or  plateau[case_bot]== plateau[1]==plateau[4]==plateau[7]\
-    or  plateau[case_bot]== plateau[2]==plateau[5]==plateau[8]\
-    or  plateau[case_bot]== plateau[0]==plateau[4]==plateau[8]\
-    or  plateau[case_bot]== plateau[6]==plateau[4]==plateau[2]:
+    if  plateau[case_bot]==plateau[0]==plateau[1]==plateau[2]\
+    or  plateau[case_bot]==plateau[3]==plateau[4]==plateau[5]\
+    or  plateau[case_bot]==plateau[6]==plateau[7]==plateau[8]\
+    or  plateau[case_bot]==plateau[0]==plateau[3]==plateau[6]\
+    or  plateau[case_bot]==plateau[1]==plateau[4]==plateau[7]\
+    or  plateau[case_bot]==plateau[2]==plateau[5]==plateau[8]\
+    or  plateau[case_bot]==plateau[0]==plateau[4]==plateau[8]\
+    or  plateau[case_bot]==plateau[6]==plateau[4]==plateau[2]:
         win=False
     while win != True or win != False or 0 in plateau:
-        tab[demande joueur] = 1
-        verifie si le joueur a gagné
+        choix_joueur
+         
+        """verifie si le joueur a gagné
             si oui : win = true
         sinon : tab[bot] = 2
         verifie si le bot a gagné 
-            si oui : win = false
+            si oui : win = false"""
 
-if win == true:
-return Le joueur a gagné !
-if win == false:
-return le joueur a perdu !
-else:
-return égalité !
+#if win == true:
+#return "Le joueur a gagné !"
+#if win == false:
+#return "le joueur a perdu !"
+#else:
+#return "égalité !"
